405032ae62b7b99453af571f3f47d829f9f5483a
MichaelLukaszyk <ml168@illinois.edu>
2026-04-07 17:57:46 -0500

    test

--- emulator/grid_run/run_tardis.py
+++ emulator/grid_run/run_tardis.py
@@ -151,6 +151,9 @@ def log_sim(params, sim):
     params['iterations'] = str(sim.iterations_executed) + '/' + str(sim.iterations)
     v, tau_thomson, tau_expansion, tau_planck, tau_rosseland = get_tau(sim, params['t_exp'].value)
     v_phot, T_phot = get_phot(sim, v, tau_rosseland)
+    print('t_exp:', params['t_exp'].value)
+    print('v_phot:', v_phot)
+    print('T_phot:', T_phot)
     params['v_phot'] = v_phot
     params['T_phot'] = T_phot
     params['T_inner'] = sim.plasma.t_rad[0]
